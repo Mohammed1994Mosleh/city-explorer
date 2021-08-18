137a40bac9d8ee66c6a6bc387697631b9e4b496e
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-08-19 01:38:15 +0300

    lab07 front end complete just image

--- src/component/Location.js
+++ src/component/Location.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
@@ -17,8 +16,10 @@ class Location extends React.Component{
            cityName:'',
            datLOcation:{},
            showData:false,
-           map:'',
-           weather:[]
+          //  map:'',
+           weather:[],
+           ccityWeatherandmovie:'',
+           movies:[]
        }
 
    }
@@ -30,39 +31,35 @@ class Location extends React.Component{
         await this.setState({
             cityName:e.target.city.value
         })
-
+        // https://eu1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
         let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.cityName}&format=json`;
         let wearUrl=`${process.env.REACT_APP_SERVER_LINK}/weather?searchQuery=${this.state.cityName}`
-
-
-        
+        let resultData = await axios.get(locURL);
+        let wethData=await axios.get(wearUrl);
+        // console.log(resultData);
+        // console.log(wethData);
        
-       
-        
-   
-         let resultData = await axios.get(locURL);
-
-         let wethData=await axios.get(wearUrl);
-
          await this.setState({
            datLOcation:resultData.data[0],
+           weather:wethData,
            
-         
-           map:`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.datLOcation.lat},${this.state. datLOcation.lon}&zoom=13&size=400x400&format=jpeg&maptype=roadmap&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`,
-           
-           
+            
+           })
+           console.log(this.state.datLOcation)
 
-        })
+           
+     console.log(this.state.datLOcation.lat);
+          let map=`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.datLOcation.lat},${this.state. datLOcation.lon}&zoom=13&size=400x400&format=jpeg&maptype=roadmap&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`;
+          console.log(map);
+          await this.setState({
+            showData:true
+           })
+          
         
+        console.log(this.state.datLOcation);
+        console.log('fdgfdsd',this.state.weather);
+
         
-        await this.setState({
-          weather:wethData,
-
-          showData:true
-        })
-
-
-        // console.log(this.state.weather);
         
       
    
@@ -71,16 +68,9 @@ class Location extends React.Component{
    
        }
 
-
-       handleClose=async()=>{
-        await this.setState({
-           
-            showData:false
- 
-         })
+       
 
 
-       }
    
 
 render(){
@@ -97,87 +87,23 @@ return(
         <button>Explore</button>
      </form>
 
-     {this.state.showData &&
-             <Card style={{ width: '18rem' }}>
-             <Card.Img variant="top" src={this.state.map} />
-           <Card.Body>
-             <Card.Title>{this.state.cityName}</Card.Title>
-             <Card.Text>
-                   Lat:{this.state. datLOcation.lat} /Lon:{this.state. datLOcation.lon}!
-                        {console.log(this.state.weather.data)}
-                        {console.log(this.state.weather.data[0].date)}
-
-                        {this.state.weather.data.map((item,ind) =>{
-                            return(
-                              <>
-                                  <p key={ind}>{item.date} 
-                                       {item.lowtemp} 
-                                       {item.maxtemp}                                 
-                                  </p>
-                              </>
-                            )
 
 
-                        } )}
 
-                     {/* <p>{this.state.weather.data} </p> */}
+
+    {this.state.showData &&
+
+    <>
+    <p>{this.state.cityName}</p> 
+    <p>  Lat:{this.state.datLOcation.lat}</p>
+    <p>Lon:{this.state.datLOcation.lon}</p>
+    <img src={this.map} />
+
+    </>
  
-             </Card.Text>
-             <Button variant="primary">Go somewhere</Button>
-           </Card.Body>
-         </Card>
-          }
-
-        
-
-      {/* <Modal show={this.state.showData} >
-
-
-      {console.log(this.state.weather[1])}
-         
-          <Modal.Header closeButton>
-               <Modal.Title>{this.state.cityName}</Modal.Title>
-          </Modal.Header>
-          <img src={this.state.map} />
-
-          
-
-          <Modal.Body>Lat:{this.state. datLOcation.lat} /Lon:{this.state. datLOcation.lon}!
-
-           <p>{this.state.weather.discription} </p>
-          
-          
-          </Modal.Body>
-
-          
-          <Modal.Footer>
-             <Button variant="secondary" onClick={this.handleClose}>
-               Close
-             </Button>
-           
-          </Modal.Footer>
-      </Modal> */}
-
-
-
-      
-
-          {/* {this.state.showData &&
-            <p>{this.state.searchCity} Lat:{this.state. datLOcation.lat} /Lon:{this.state. datLOcation.lon} </p>
-          } */}
-
-        {/* <p>{this.state.searchCity} Lat:{this.state. datLOcation.lat} /Lon:{this.state. datLOcation.lon} </p> */}
-
-</>
-
-
-
-)
-
-
-
-}
-
-}
+ 
+ }
+   
+</>)}}
 
 export default Location
